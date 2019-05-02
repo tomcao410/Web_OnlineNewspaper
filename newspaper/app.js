3fe441761281ee54e31a014e2e87df6ad055f9c3
1612082 <38983606+1612082@users.noreply.github.com>
2019-05-03 00:46:55 +0700

    update data

--- newspaper/app.js
+++ newspaper/app.js
@@ -42,5 +42,5 @@ app.use(function(err, req, res, next) {
 app.locals.datathoisu = require('./model/data.json');
 app.locals.dataTopic = require('./model/topic.json');
 app.locals.dataTopten = require('./model/topten.json');
-
+app.locals.datathreepost = require('./model/threepost.json');
 module.exports = app;
