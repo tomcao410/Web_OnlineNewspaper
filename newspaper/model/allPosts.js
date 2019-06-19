09e6586012d390db6b370ff2364d24a0e5aa78fe
1612082 <38983606+1612082@users.noreply.github.com>
2019-06-20 02:11:25 +0700

    update premium
    sửa chữa bài viết có primeum

--- newspaper/model/allPosts.js
+++ newspaper/model/allPosts.js
@@ -1,11 +1,11 @@
 var db = require('../utils/db');
 module.exports = {
     all : () => {
-        return db.load("select P.id, P.title, C.categoryName, SC.subCategoryName, date(P.publishDate) as publishDate, P.postExcerpt, P.content, P.views, P.imgLink, U.fullname from Posts as P, SubCategories as SC, Categories as C, Writer as W, Users as U where P.category = C.id and P.sub_category = SC.id and SC.categoryId = P.category and W.id = P.authorId and U.id = W.id order by P.publishDate desc");
+        return db.load("select P.id, P.title, C.categoryName, SC.subCategoryName, date(P.publishDate) as publishDate, P.postExcerpt, P.content, P.views, P.imgLink, U.fullname, P.approval, p.premium from Posts as P, SubCategories as SC, Categories as C, Writer as W, Users as U where P.category = C.id and P.sub_category = SC.id and SC.categoryId = P.category and W.id = P.authorId and U.id = W.id order by P.publishDate desc");
     },
 
     pageBy : (limit, offset) => {
-        return db.load(`select P.*, C.categoryName, SC.subCategoryName, U.fullname from Posts as P, Categories as C, SubCategories as SC, Writer as W, Users as U where P.category = C.id and P.sub_category = SC.id and SC.categoryId = P.category and W.id = P.authorId and U.id = W.id order by P.publishDate desc limit ${limit} offset ${offset}`);
+        return db.load(`select P.*, C.categoryName, SC.subCategoryName, U.fullname from Posts as P, Categories as C, SubCategories as SC, Writer as W, Users as U where P.category = C.id and P.sub_category = SC.id and SC.categoryId = P.category and W.id = P.authorId and U.id = W.id order by P.premium DESC, P.publishDate desc limit ${limit} offset ${offset}`);
     },
     pageByCat : (catName, limit, offset) => {
         return db.load(`select P.*, C.categoryName, SC.subCategoryName, U.fullname from Posts as P, Categories as C, SubCategories as SC, Writer as W, Users as U where C.categoryName = "${catName}" and P.sub_category = SC.id and SC.categoryId = P.category and C.id = P.category and W.id = P.authorId and U.id = W.id order by P.publishDate desc limit ${limit} offset ${offset}`);
